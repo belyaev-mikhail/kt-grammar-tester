52843b52910511e5df2aceb693b831862d2a7e97
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-07-11 16:08:40 +0700

    Save result

--- src/main/kotlin/ru/nsu/tester/Main.kt
+++ src/main/kotlin/ru/nsu/tester/Main.kt
@@ -1,14 +1,18 @@
 package ru.nsu.tester
 
 import ru.nsu.tester.comparison.Comparator
-import ru.nsu.tester.parsing.ParsingOverview
 import ru.nsu.tester.gui.AnalysisRenderer
-import java.io.File
+import ru.nsu.tester.parsing.ParsingOverview
 import java.awt.EventQueue
+import java.io.File
 
 fun main(args: Array<String>) {
     val dir = File(if (args.isNotEmpty()) args[0] else ".")
     assert(dir.exists() && dir.isDirectory)
+
+    val resultFile = File("kotoed-result.md")
+    resultFile.writeText("File | Precision | Recall | F-score\n")
+    resultFile.appendText(":----:|:----:|:----:|:----:\n")
 
     var correctCount = 0
     var totalCount = 0
@@ -20,7 +24,8 @@ fun main(args: Array<String>) {
                 result.output()
                 if (result.isCorrect()) {
                     val compareResult = Comparator.inspectTree(it.path, result.root!!)
-                    compareResult.output()
+                    compareResult.consoleOutput()
+                    resultFile.appendText(compareResult.fileOutput(it) + "\n")
                     if (compareResult.isCorrect()) correctCount++
                     /* Uncomment to view erroneous subtree */
                     /*
